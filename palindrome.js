a2df002350ecd8aa08caddedfd1daf800c7e1f65
Dana L. Brown <dana.l.brown@gmail.com>
2019-11-15 09:55:49 -0500

    Update palindrome.js

--- palindrome.js
+++ palindrome.js
@@ -1,10 +1,11 @@
 //palindrome is a word or sentence that reads the same backwards and forwards
 const palindrome = str => {
- // make the string lowercase
- str = str.toLowerCase()
+   // make the string lowercase
+   str = str.toLowerCase()
    // this is best accomlished by turning the string into an array, reversing it, making our comparison and then put it back together
- return str === str.split('').reverse().join('')
-}
+   return str === str.split('').reverse().join('')
+ }
+
 /* in console, test the code with the following:
 palindrome ("Abba");
 palindrome ("Anna");
