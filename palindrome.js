264ad02acfdf13ecc4d4e854872b5f208c8950a3
Dana L. Brown <dana.l.brown@gmail.com>
2019-11-15 09:55:05 -0500

    Update palindrome.js

--- palindrome.js
+++ palindrome.js
@@ -2,10 +2,8 @@
 const palindrome = str => {
  // make the string lowercase
  str = str.toLowerCase()
- 
-  // this is best accomlished by turning the string into an array, reversing it, making our comparison and then put it back together
+   // this is best accomlished by turning the string into an array, reversing it, making our comparison and then put it back together
  return str === str.split('').reverse().join('')
-
 }
 /* in console, test the code with the following:
 palindrome ("Abba");
